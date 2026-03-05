3026404985eab83b6f7119889b9e78eac214d50c
BauntyL <bauntyxr@mail.ru>
2026-03-05 22:32:14 +0300

    Fix-ts-ignore-import

--- api/auth/register.ts
+++ api/auth/register.ts
@@ -1,4 +1,5 @@
-import { db } from "../../lib/db.ts";
+// @ts-ignore
+import { db } from "../../lib/db";
 import { users, type InsertUser } from "@shared/schema";
 import { eq } from "drizzle-orm";
 import type { VercelRequest, VercelResponse } from '@vercel/node';
